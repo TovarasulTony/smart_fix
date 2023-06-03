0b01e9e00ca01d3bb129ff4d90bbf8f8b18fed25
Chunchunmarx <andreeaoprescu12@yahoo.com>
2023-06-03 12:04:34 +0300

    update

--- test.py
+++ test.py
@@ -2,6 +2,11 @@
 import datetime
 
 var = datetime.datetime.now()
+var = str(var)
 
+print("-------------")
 print(var)
 print(type(var))
+
+exe = var[:3] + var[5:6] + var[8:9] + '-' var[10:]
+print(exe)
